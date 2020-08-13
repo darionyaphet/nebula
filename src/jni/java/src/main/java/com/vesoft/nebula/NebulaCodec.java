ac951a86a78eb8929ece4c8b41b3fd174492d7eb
darionyaphet <darion.yaphet@gmail.com>
2020-08-13 12:31:54 +0800

    make constant public

--- src/jni/java/src/main/java/com/vesoft/nebula/NebulaCodec.java
+++ src/jni/java/src/main/java/com/vesoft/nebula/NebulaCodec.java
@@ -50,21 +50,21 @@ public class NebulaCodec {
         }
     }
 
-    private static final int PARTITION_ID_SIZE = 4;
-    private static final int VERTEX_ID_SIZE = 8;
-    private static final int TAG_ID_SIZE = 4;
-    private static final int TAG_VERSION_SIZE = 8;
-    private static final int EDGE_TYPE_SIZE = 4;
-    private static final int EDGE_RANKING_SIZE = 8;
-    private static final int EDGE_VERSION_SIZE = 8;
-    private static final int VERTEX_SIZE = PARTITION_ID_SIZE + VERTEX_ID_SIZE
+    public static final int PARTITION_ID_SIZE = 4;
+    public static final int VERTEX_ID_SIZE    = 8;
+    public static final int TAG_ID_SIZE       = 4;
+    public static final int TAG_VERSION_SIZE  = 8;
+    public static final int EDGE_TYPE_SIZE    = 4;
+    public static final int EDGE_RANKING_SIZE = 8;
+    public static final int EDGE_VERSION_SIZE = 8;
+    public static final int VERTEX_SIZE = PARTITION_ID_SIZE + VERTEX_ID_SIZE
         + TAG_ID_SIZE + TAG_VERSION_SIZE;
-    private static final int EDGE_SIZE = PARTITION_ID_SIZE + VERTEX_ID_SIZE
+    public static final int EDGE_SIZE   = PARTITION_ID_SIZE + VERTEX_ID_SIZE
         + EDGE_TYPE_SIZE + EDGE_RANKING_SIZE + VERTEX_ID_SIZE + EDGE_VERSION_SIZE;
 
-    private static final int DATA_KEY_TYPE = 0x00000001;
-    private static final int TAG_MASK      = 0xBFFFFFFF;
-    private static final int EDGE_MASK     = 0x40000000;
+    public static final int DATA_KEY_TYPE = 0x00000001;
+    public static final int TAG_MASK      = 0xBFFFFFFF;
+    public static final int EDGE_MASK     = 0x40000000;
 
     public static byte[] createEdgeKey(int partitionId, long srcId, int edgeType,
                                        long edgeRank, long dstId, long edgeVersion) {
